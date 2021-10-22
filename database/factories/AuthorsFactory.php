3b32f4474678e4cd623e57deebe8b0c9796bec0e
Juturu Charan Sai <charan.juturu@thoughtworks.com>
2021-10-22 15:49:27 +0530

    Harsha|Regina|Swati|Jothee|Charan Add. First Commit

---
+++ database/factories/AuthorsFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Authors;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AuthorsFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Authors::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
